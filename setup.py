201825c6aa61dada4661cecabe84374a9f34523a
rushkock <ruchella_kock@hotmail.com>
2025-05-04 12:09:27 +0200

    setup requirements adding the repo in the install requires

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='ruchella_resume',
+    name='OOP_CV',
     version='0.0.1',    
     description='An overdone installable package that displays my CV',
     url='https://github.com/rushkock/rushkock',
@@ -9,7 +9,8 @@ setup(
     license='BSD 2-clause',
     python_requires=">=3.10",
     packages=find_packages(),
-    install_requires=['getContinent','numpy','pandas','circlify', 'plotly','fastapi','kaleido','marimo','plotly','pycountry','pycountry-convert','python-dateutil','squarify','wordcloud','markdownify'],
+    install_requires=[ 'rushkock @ git+ssh://git@github.com:rushkock/rushkock.git'
+        'getContinent','numpy','pandas','circlify', 'plotly','fastapi','kaleido','marimo','plotly','pycountry','pycountry-convert','python-dateutil','squarify','wordcloud','markdownify'],
     classifiers=[
         'Programming Language :: Python :: 3.10',
         'Intended Audience :: Recruiters',
