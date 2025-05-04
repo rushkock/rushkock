043e9915be8486c8bea3b530998a3882130a22f1
rushkock <ruchella_kock@hotmail.com>
2025-05-04 11:49:46 +0200

    setup requirements with handdefined packages

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-from setuptools import setup, find_packages
-import pkg_resources
-import pathlib
-
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
+from setuptools import setup
 
 setup(
     name='ruchella_resume',
@@ -18,7 +9,7 @@ setup(
     license='BSD 2-clause',
     python_requires=">=3.10",
     packages=['rushkock'],
-    install_requires=install_requires,
+    install_requires=['getContinent','numpy','pandas','circlify', 'plotly','fastapi','kaleido','marimo','plotly','pycountry','pycountry-convert','python-dateutil','squarify','wordcloud','markdownify'],
     classifiers=[
         'Programming Language :: Python :: 3.10',
         'Intended Audience :: Recruiters',
